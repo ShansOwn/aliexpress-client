b8ddc816fb2f2b34a764f1d50c654b91e8efab18
Yegor Lashkul <yegor.lashkul@gmail.com>
2017-10-12 14:03:21 +0300

    Processing products by ids

--- src/main/java/com/shansown/aliexpress/controller/RootController.java
+++ src/main/java/com/shansown/aliexpress/controller/RootController.java
@@ -2,6 +2,8 @@ package com.shansown.aliexpress.controller;
 
 import com.shansown.aliexpress.model.Product;
 import com.shansown.aliexpress.service.ProductService;
+import java.util.Collection;
+import java.util.HashSet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,13 +26,18 @@ public class RootController {
 
   @PostMapping("/find")
   public Mono<Long> find(@RequestBody String keyword) {
+    log.info("Find products by keyword: '{}'", keyword);
     Flux<Product> products = productService.requestByKeyword(keyword);
     return productService.saveAll(products)
         .doOnSuccess(count -> log.info("{} products found and processed with '{}' keyword", count, keyword));
   }
 
-  @GetMapping("/details")
-  public Mono<Product> details(@RequestParam Long id) {
-    return productService.requestDetails(id);
+  @PostMapping("/details")
+  public Mono<Long> details(@RequestBody Collection<Long> ids) {
+    log.info("Find products by ids: {}", ids.size());
+    Flux<Product> products = Flux.fromIterable(new HashSet<>(ids))
+        .flatMap(productService::requestDetails);
+    return productService.saveAll(products)
+        .doOnSuccess(count -> log.info("{} products found and processed with {} ids", count, ids.size()));
   }
 }
